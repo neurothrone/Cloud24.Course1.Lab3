c6981cf7b532eaf5416b41ec89368ccf95d60391
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-10-09 18:07:24 +0200

    extract out CollectionView and DataTemplate contents into separate controls

---
+++ src/Applications/Clients/GymBookingManager.Maui.Client/Src/Views/Controls/WorkoutItemView.xaml.cs
@@ -0,0 +1,9 @@
+namespace GymBookingManager.Maui.Client.Views.Controls;
+
+public partial class WorkoutItemView : ContentView
+{
+    public WorkoutItemView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Applications/Clients/GymBookingManager.Maui.Client/Src/Views/Controls/WorkoutsCollectionView.xaml.cs
@@ -0,0 +1,9 @@
+namespace GymBookingManager.Maui.Client.Views.Controls;
+
+public partial class WorkoutsCollectionView : ContentView
+{
+    public WorkoutsCollectionView()
+    {
+        InitializeComponent();
+    }
+}
